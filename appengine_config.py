f0700c0535a7f8165c41ddb188c9337f2b9acd47
root <ceran.keef@gmail.com>
2020-05-28 14:47:30 +0200

    Hide config values
    Security guys are shouting at me for exposing production values,
    fixed it

--- appengine_config.py
+++ appengine_config.py
@@ -9,10 +9,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "out.settings")
 
 #APIKEYS
 
-consumer_api = "1ab2619bb8291afc91cc211b1"
-consumer_secret = "bab291ee841bf29ae18e810"
-access_token = "7a81bbb1b22e88102931"
-access_token_secret = "ebf83401ab291af401e"
+consumer_api = "XXX"
+consumer_secret = "XXX"
+access_token = "XXX"
+access_token_secret = "XXX"
 
-slack_api_token_prefix = "xoxp-1148486360387-1141545032230-1135659087559-"
-slack_api_token_postfix = "df75c296d8d90713410457958f42fe5a"
+slack_api_token_prefix = "XXX"
+slack_api_token_postfix = "XXX"
